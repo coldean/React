4e4d1f774ef5ecfd37df9de6ab84d29bac64fbed
coldean <coldean@github.com>
2024-02-07 17:53:12 +0900

    news api start_newsItem

---
+++ news-api/src/App.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=9113cfcd40b449a8aebf543212e0531a',
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
+    </div>
+  );
+};
+
+export default App;
