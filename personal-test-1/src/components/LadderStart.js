473a0f23a7f3e43d59b0aa2ceda3c650206c0234
coldean <jisung0603@gmail.com>
2024-02-20 10:16:07 +0900

    made ladder route find algorithm

--- personal-test-1/src/components/LadderStart.js
+++ personal-test-1/src/components/LadderStart.js
@@ -4,6 +4,8 @@ import "./Ladder.scss";
 const LadderStart = ({ count, ladderStep, deletedLines, selectedButton }) => {
   const [verLadder, setVerLadder] = useState([]);
   const [horLadder, setHorLadder] = useState([]);
+  const [routeHor, setRouteHor] = useState([]);
+  const [routeVer, setRouteVer] = useState([]);
 
   const followingHor = [];
   for (let i = 0; i < ladderStep; i++) {
@@ -37,39 +39,40 @@ const LadderStart = ({ count, ladderStep, deletedLines, selectedButton }) => {
 
   // 선택된 라인 따라가는 함수
   useEffect(() => {
-    const followLine = () => {
-      // 가로선
-      var curVertical = selectedButton;
-      for (let i = 0; i < ladderStep; i++) {
-        if (curVertical > 0 && deletedLines[i][curVertical - 1]) {
-          followingHor[i][curVertical - 1] = true;
-          curVertical -= 1;
-        } else if (curVertical < count - 2 && deletedLines[i][curVertical]) {
-          followingHor[i][curVertical] = true;
-          curVertical += 1;
-        } else {
-          // 가로가 없을 때. 왼쪽 끝, 오른쪽 끝인 경우도 포함
-          // 아마 아무것도 안해도 될 듯 함.
-        }
+    // 가로선
+    var curVertical = selectedButton;
+    for (let i = 0; i < ladderStep; i++) {
+      if (curVertical > 0 && !deletedLines[i][curVertical - 1]) {
+        followingHor[i][curVertical - 1] = true;
+        curVertical -= 1;
+      } else if (curVertical < count - 1 && !deletedLines[i][curVertical]) {
+        followingHor[i][curVertical] = true;
+        curVertical += 1;
+      } else {
+        // 가로가 없을 때. 왼쪽 끝, 오른쪽 끝인 경우도 포함
+        // 아무것도 안해도 될 듯 함.
       }
+    }
 
-      // 세로선
-      curVertical = selectedButton;
-      followingVer[0][curVertical] = true;
-      for (let i = 1; i < ladderStep + 1; i++) {
-        if (curVertical > 0 && deletedLines[i][curVertical - 1]) {
-          followingVer[i][curVertical - 1] = true;
-          curVertical -= 1;
-        } else if (curVertical < count - 1 && deletedLines[i][curVertical]) {
-          followingVer[i][curVertical + 1] = true;
-          curVertical += 1;
-        } else {
-          // 가로가 없을 때. 왼쪽 끝, 오른쪽 끝인 경우도 포함
-          followingVer[i][curVertical] = true;
-        }
+    // 세로선
+    curVertical = selectedButton;
+    followingVer[0][curVertical] = true;
+    for (let i = 1; i < ladderStep + 1; i++) {
+      if (curVertical > 0 && !deletedLines[i - 1][curVertical - 1]) {
+        followingVer[i][curVertical - 1] = true;
+        curVertical -= 1;
+      } else if (curVertical < count - 1 && !deletedLines[i - 1][curVertical]) {
+        followingVer[i][curVertical + 1] = true;
+        curVertical += 1;
+      } else {
+        // 가로가 없을 때. 왼쪽 끝, 오른쪽 끝인 경우도 포함
+        followingVer[i][curVertical] = true;
       }
-    };
-  });
+    }
+
+    setRouteHor(followingHor);
+    setRouteVer(followingVer);
+  }, [count, deletedLines, ladderStep, selectedButton]);
 
   return (
     <div>
@@ -78,15 +81,23 @@ const LadderStart = ({ count, ladderStep, deletedLines, selectedButton }) => {
           {verLadder.map(({ id: verId }) => (
             <div key={verId}>
               {horId === ladderStep ? ( // 세로
-                <div className="Ladder-Vertical-last"></div>
+                <div
+                  className={`Ladder-Vertical-last ${
+                    routeVer[horId][verId] ? "selected" : ""
+                  }`}
+                ></div>
               ) : (
-                <div className="Ladder-Vertical"></div>
+                <div
+                  className={`Ladder-Vertical ${
+                    routeVer[horId][verId] ? "selected" : ""
+                  }`}
+                ></div>
               )}
 
               {verId === count - 1 || horId === ladderStep ? null : ( // 가로
                 <div
                   className={`Ladder-Horizontal ${
-                    followingHor[horId][verId]
+                    routeHor[horId][verId]
                       ? "selected"
                       : deletedLines[horId][verId]
                       ? "deleted"
